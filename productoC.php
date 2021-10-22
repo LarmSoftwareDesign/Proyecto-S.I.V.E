b4bbdd3e64eaa08bcd1afe80c2dfcfc8a58e2152
Jostman Sanchez <jostmanasm@gmail.com>
2021-10-22 07:46:42 -0300

    correciones parte 9

--- productoC.php
+++ productoC.php
@@ -76,17 +76,33 @@
                 }else{
                   echo "<h4>Disponibilidad: <span style=\"color:green;\">En Stock </span></h4>";
                 }
-                echo "<h5>Marca: <a href = \"#\">".$NCE['Nomempresa']."</a></h5>";
+                echo "<h5>Marca: <span style = \"color: skyblue\">".$NCE['Nomempresa']."</span></h5>";
                 echo "<h5>Nacionalidad: ".$producto['Nacionalidad']."</h5>";
               ?>
+              <h5><?php echo "Condicion: ".$producto['Condicion'];?></h5>
             </div>
             
 
           </div>
-          <div class="col-auto" id= "c3">
-            <h2><?php echo "Precio:".$producto['Precio'];?></h2>
-            <h5></h5>
+          <div class="col-auto" id= "c3" style="text-align: center;">
+            
+            <h2><?php echo "Costo: ".$producto['Precio'];?></h2>
+            <br>
+            <p style ="font-size:24px;"><?php echo $producto['Descripcion'];?></p>
+            <div class="row">
+              <form action="" method="get">
+              <button class="btn btn-outline-info btn-lg">
+                  <svg src="bootstrap-5.1.0-dist/SVG/bag-plus.svg" width="32" height="32" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                  </svg>
+                 Agregar al carrito
+              </button>
+                
+              </form>
+            </div>
           </div>
+
         </div>
       </div>
     </section>
